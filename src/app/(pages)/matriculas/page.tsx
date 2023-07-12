335c469e5e5f1738add18a572f769664ddd58375
eduardocassano.sa <eduardo.sa@rio.rj.gov.br>
2023-07-12 15:45:12 -0300

    Alteracoes matricula

--- src/app/(pages)/matriculas/page.tsx
+++ src/app/(pages)/matriculas/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import Banner from "@/app/components/Banner/Banner";
 import Button from "@/app/components/Button/Button";
-import Matricula from "@/app/components/Matricula/Matricula";
+import { useState } from "react";
 import styles from "./matriculas.module.css";
+import { MdStars } from "react-icons/md";
+import { AiFillStar } from "react-icons/ai";
 
 type Matricula = {
     id: number;
@@ -10,9 +12,17 @@ type Matricula = {
     nome: string;
 }
 
-const matriculas: Matricula[] = [{ id: 1, matricula: "1234567", nome: "Eduardo" }, { id: 1, matricula: "1234567", nome: "Eduardo" }, { id: 1, matricula: "1234567", nome: "Eduardo" }, { id: 1, matricula: "1234567", nome: "Eduardo" }];
+const matriculas: Matricula[] = [
+    { id: 1, matricula: "1234567", nome: "Sofia Rodriguez" },
+    { id: 2, matricula: "1234567", nome: "Lucas Thompson" },
+    { id: 3, matricula: "1234567", nome: "Isabella Parker" },
+    { id: 4, matricula: "1234567", nome: "Gabriel Anderson" },
+    { id: 5, matricula: "1234567", nome: "Valentina Mitchell" },
+    { id: 6, matricula: "1234567", nome: "Benjamin Campbell" }
+];
 
 const Matriculas = () => {
+    const [matriculaSelected, setMatriculaSelected] = useState<number>()
     return (
         <div className={styles.main}>
             <Banner type="overlaySM" banner="bannerMatriculas">
@@ -21,14 +31,23 @@ const Matriculas = () => {
             <div className={styles.container}>
                 <h2 className={styles.title}>Minhas Matrículas</h2>
                 <p className={styles.text}>Consulte as matrículas e veja os boletins escolares.</p>
-                <div className={styles.matriculas}>
+                <div className={styles.matriculasContainer}>
                     {matriculas.length === 0 ? <h3>No momento você não possui matrícula cadastrada. Insira os dados
                         da matrícula e a data de nascimento do aluno e clique em salvar</h3>
                         :
-                        <div>{matriculas.map(({ id, nome, matricula }: Matricula) => (<Matricula nome={nome} matricula={matricula} key={id} />))}</div>}
+                        <div className={styles.matriculas}>
+                            {matriculas.map(({ id, nome, matricula }: Matricula) => (
+                                <span className={id === matriculaSelected ? styles.matriculaSelected : styles.matricula} key={id} onClick={() => setMatriculaSelected(id)}>
+                                    {id === matriculaSelected ? <span className={styles.star}><AiFillStar size={20} /></span> : ""}
+                                    <h4 className={styles.matriculaTitle}>{nome}</h4>
+                                    <p className={styles.matriculaNumber}>{matricula}</p>
+                                </span>
+                            ))}
+                        </div>
+                    }
                 </div>
                 <div className={styles.cadastrarMatricula}>
-                    <h3>Incluir nova matrícula</h3>
+                    <h3 className={styles.title2}>Incluir nova matrícula</h3>
                     <form action="">
                         <label htmlFor="">Matrícula:</label>
                         <input type="text" placeholder="Ex: 1234567" className={styles.input} />
