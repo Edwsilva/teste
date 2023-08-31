2ad6ba6e7ab6c6e4f395e192c8d8858f28025861
Edwilson da Silva <edwilson.silva@rio.rj.gov.br>
2023-08-31 16:28:58 -0300

    Keycloak configuração inicial

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -1,14 +1,22 @@
 "use client";
 import styles from "./login.module.css";
 import { BiSolidUserCircle } from "react-icons/bi";
+import userKeycloak from '../../../hooks/userHookKeycloak';
 
 const Login = () => {
+  // const keycloak = (callBack: any) => {
+  //   callBack({ checkLoginIframe: false });
+  // };
+  // ().init({
+  //     onLoad: initOptions.onLoad, checkLoginIframe: false
+  //   });
+
+  const kc = () => {
+    userKeycloak.doInitialize.init({ checkLoginIframe: false });
+  };
   return (
-    <button
-      className={styles.button}
-      onClick={() => console.log("Login Component")}
-    >
-      <BiSolidUserCircle className={styles.icon}/>
+    <button className={styles.button} onClick={() => kc()}>
+      <BiSolidUserCircle className={styles.icon} />
       Login
     </button>
   );
---
+++ src/hooks/userHookKeycloak.js
@@ -0,0 +1,97 @@
+'use client';
+import React, { useState, useEffect, useRef } from 'react';
+import Keycloak from 'keycloak-js';
+
+// const [isLogin, setLogin] = useState(false);
+// const [token, setToken] = useState(null);
+
+// VITE_KEYCLOAK_URL=http://127.0.0.1:4000/
+// VITE_KEYCLOAK_REALM=myrealm
+// VITE_KEYCLOAK_CLIENT=myclient
+
+let initOptions = {
+  url: 'http://127.0.0.1:4000/',
+  realm: 'myrealm',
+  clientId: 'myclient',
+  onLoad: 'login-required',
+  // KeycloakResponseType: 'code'
+};
+
+// const client = new Keycloak({
+//   url: process.env.VITE_KEYCLOAK_URL,
+//   realm: process.env.VITE_KEYCLOAK_REALM,
+//   clientId: process.env.VITE_KEYCLOAK_CLIENT,
+//   onLoad: 'login-required',
+// });
+
+const _kc = new Keycloak(initOptions);
+console.log('KCCC', _kc);
+
+/**
+ * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
+ *
+ * @param onAuthenticatedCallback
+ */
+const initKeycloak = (onAuthenticatedCallback) => {
+  console.log(onAuthenticatedCallback);
+  // const isRun = useRef(false);
+  // const [isLogin, setLogin] = useState(false);
+  // const [token, setToken] = useState(null);
+  //   useEffect(() => {
+  //     console.log('YESYE');
+  //     _kc
+  //       .init({
+  //         onLoad: initOptions.onLoad,
+  //         checkLoginIframe: false,
+  //         // onLoad: 'check-sso',
+  //         // silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
+  //         // pkceMethod: 'S256',
+  //       })
+  //       .then((authenticated) => {
+  //         if (!authenticated) {
+  //           console.log('user is not authenticated..!');
+  //         }
+  //         onAuthenticatedCallback();
+  //       })
+  //       .catch(console.error);
+  //   }, []);
+  // };
+};
+
+const doInitialize = _kc;
+// const doInitialize = _kc.init({
+//   onLoad: initOptions.onLoad,
+//   checkLoginIframe: false,
+// });
+
+const doLogin = _kc.login;
+
+const doLogout = _kc.logout;
+
+const getToken = () => _kc.token;
+
+const getTokenParsed = () => _kc.tokenParsed;
+
+const isLoggedIn = () => !!_kc.token;
+
+const updateToken = (successCallback) =>
+  _kc.updateToken(5).then(successCallback).catch(doLogin);
+
+const getUsername = () => _kc.tokenParsed?.preferred_username;
+
+const hasRole = (roles) => roles.some((role) => _kc.hasRealmRole(role));
+
+const userHookKeycloak = {
+  initKeycloak,
+  doInitialize,
+  doLogin,
+  doLogout,
+  isLoggedIn,
+  getToken,
+  getTokenParsed,
+  updateToken,
+  getUsername,
+  hasRole,
+};
+
+export default userHookKeycloak;
