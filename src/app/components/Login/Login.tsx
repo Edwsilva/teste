28afbd1d9a100bc16856a75a065c39714808418e
Edwilson da Silva <edwilson.silva@rio.rj.gov.br>
2023-09-11 18:46:20 -0300

    Ajuste parcial do componente login

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -3,47 +3,43 @@ import styles from "./login.module.css";
 import { BiSolidUserCircle } from "react-icons/bi";
 import userKeycloak from '../../../hooks/userHookKeycloak';
 import { useState } from "react";
-
+import { userAgent } from 'next/server';
 
 const Login = () => {
-  console.log("LOGINN")
-  const teste = true
-  const [autenticado, setAutenticado] = useState(false)
-  // const keycloak = (callBack: any) => {
-  //   callBack({ checkLoginIframe: false });
-  // };
-  // ().init({
-  //     onLoad: initOptions.onLoad, checkLoginIframe: false
-  //   });
+  console.log('LOGINN');
 
+  const [user, setUser] = useState('Login');
+  const [autenticado, setAutenticado] = useState(false);
   const kc = async () => {
     try {
-      console.log("BLOCO TRY")
-      if(autenticado) {
-        const token = userKeycloak.getToken()
-
-        console.log("TOKEN ", token)
-        console.log("UserName ", userKeycloak.getUsername())
-        const role = await userKeycloak.hasRole(["admin", "teste"])
-        console.log("HasHole ", role)
+      console.log('BLOCO TRY');
+      if (autenticado) {
+        const token = userKeycloak.getToken();
+        console.log('TOKEN ', token);
+        console.log('UserName ', userKeycloak.getUsername());
+        const role = await userKeycloak.hasRole(['admin', 'teste']);
+        console.log('HasHole ', role);
         //return null
       }
-      const authenticated = await userKeycloak.doInitialize.init({onLoad: "login-required"})
-      if(authenticated) {
-        alert(userKeycloak.getToken())
+      const authenticated = await userKeycloak.doInitialize.init({
+        onLoad: 'login-required',
+      });
+      if (authenticated) {
+        console.log('USER NAME ', user);
+        setUser(userKeycloak.getUsername());
+        console.log('CHANGED NAME ', user);
+        setAutenticado(authenticated);
       }
-      console.log("authenticated", authenticated)
+      console.log('authenticated', authenticated);
       alert(authenticated ? 'authenticated' : 'not authenticated');
-      setAutenticado(authenticated)
     } catch (error) {
       alert('failed to initialize');
     }
-  
   };
   return (
     <button className={styles.button} onClick={() => kc()}>
       <BiSolidUserCircle className={styles.icon} />
-      Login
+      {user}
     </button>
   );
 };
