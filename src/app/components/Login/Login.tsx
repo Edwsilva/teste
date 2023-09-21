8fd146cae7cdeec4d93332ee212b758eae0a2eab
Edwilson da Silva <edwilson.silva@rio.rj.gov.br>
2023-09-21 11:24:37 -0300

    Ajuste das telas de login e navbar

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -1,6 +1,14 @@
-"use client";
-import styles from "./login.module.css";
-import { BiSolidUserCircle } from "react-icons/bi";
+'use client';
+import {
+  Dispatch,
+  SetStateAction,
+  useState,
+  useEffect,
+  ReactNode,
+} from 'react';
+import styles from './login.module.css';
+import { BiSolidUserCircle } from 'react-icons/bi';
+import { MdOutlineLogout } from 'react-icons/md';
 
 import { KeycloakInitOptions } from 'keycloak-js';
 import userHookkeycloak from '../../../hooks/userHookKeycloak';
@@ -8,39 +16,67 @@ import { useDispatch } from 'react-redux';
 import { AppDispatch, useAppSelector } from '@/redux/store';
 import { authActions } from '@/redux/features/auth-slice';
 
-import { useState, useEffect, ReactNode, useRef } from 'react';
-import { userAgent } from 'next/server';
 import { UserInfo } from '@/app/utils/types';
 
 //const initOptions: KeycloakInitOptions = {onLoad: "login-required"};
 
 const initOptions: KeycloakInitOptions = {};
 
-const Login = () => {
-  
+const name = 'Eduardo Cassano de Sá';
+
+type LoginProps = {
+  mobile?: boolean;
+  menuOpen?: boolean;
+  linksLength?: number;
+  delay?: number;
+  loginState: boolean;
+  setLoginState: Dispatch<SetStateAction<boolean>>;
+};
+
+const Login = ({
+  mobile,
+  menuOpen,
+  linksLength,
+  delay,
+  loginState,
+  setLoginState,
+}: LoginProps) => {
+  const animStyle =
+    linksLength && delay
+      ? { animationDuration: `${linksLength * delay}s` }
+      : {};
+
+  const handleLogin = () => {
+    userHookkeycloak.doLogin();
+  };
+
+  const handleLogout = () => {
+    dispatch(authActions.setLogOut());
+    setLoginState(false);
+    // resetUserToken();
+    userHookkeycloak.doLogout();
+  };
+
   const dispatch = useDispatch<AppDispatch>();
-  const {authenticated, userInfo} = useAppSelector((state) => state.authUser);
-  
+  const { authenticated, userInfo } = useAppSelector((state) => state.authUser);
+
   const [user, setUser] = useState<UserInfo | ReactNode>();
- 
+
   const [userIsAuthenticated, setUserIsAuthenticated] = useState(false);
 
-  if(userInfo.token ==="") {
-
+  if (userInfo.token === '') {
     userHookkeycloak.doInitialize.init(initOptions).success((auth) => {
-      console.log("AUTH ", auth)
+      console.log('AUTH ', auth);
       try {
         if (auth) {
-  
           // @ts-ignore
           const userEmail = userHookkeycloak.getTokenParsed()?.email;
           // @ts-ignore
-          const userName = userHookkeycloak.getTokenParsed()?.name;
+          const userName = userHookkeycloak.getTokenParsed()?.given_name;
           const userToken = userHookkeycloak.getToken();
-  
-  
+
           console.log('KEYCLOAK ', userEmail);
-  
+
           console.log('KEYCLOAK ', userHookkeycloak.getToken());
           // setUser(keycloak.profile?.email);
           // console.log('USER ', keycloak.profile?.email);
@@ -53,27 +89,52 @@ const Login = () => {
           dispatch(
             authActions.setLogIn({
               authenticated: auth,
-              userInfo: { name: userName, email: userEmail, token: userToken  },
+              userInfo: { name: userName, email: userEmail, token: userToken },
             })
           );
-  
         } else {
-          console.log("USUARIO NÂO AUTETICADO")
+          console.log('USUARIO NÂO AUTETICADO');
         }
-  
-  
       } catch (error) {
         console.error('Erro ao iniciar a autenticação:', error);
       }
-      
-    })
+    });
   }
 
   return (
-    <button className={styles.button} onClick={() => userHookkeycloak.doLogin()}>
-      <BiSolidUserCircle className={styles.icon} />
-      {userInfo?.name}
-    </button>
+    <>
+      {/* {loginState ? ( */}
+      {authenticated ? (
+        <div className={styles.logged}>
+          <span className={styles.name}>
+            {/* Olá, {name.slice(0, name.indexOf(' '))} */}
+            Olá, {userInfo?.name}
+          </span>
+          <button
+            className={styles.logout}
+            // onClick={() => setLoginState(false)}
+            onClick={handleLogout}
+          >
+            <MdOutlineLogout size={25} />
+          </button>
+        </div>
+      ) : (
+        <button
+          style={animStyle}
+          className={
+            mobile
+              ? `${styles.button} ${styles.buttonMobile} ${
+                  menuOpen ? styles.menuOpenAnim : ''
+                }`
+              : styles.button
+          }
+          onClick={handleLogin}
+        >
+          <BiSolidUserCircle className={styles.icon} />
+          Login
+        </button>
+      )}
+    </>
   );
 };
 
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,101 +1,108 @@
-"use client";
-import styles from "./navbar.module.css";
-import Image from "next/image";
-import Link from "next/link";
-
+'use client';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { AppDispatch, useAppSelector } from '@/redux/store';
 import { authActions } from '@/redux/features/auth-slice';
+import userHookKeycloak from '../../../hooks/userHookKeycloak';
 
-import { usePathname } from "next/navigation";
-import logo from "../../../../public/images/logoNovo.png";
-import Login from "../Login/Login";
-import { useEffect, useState } from "react";
-import { IoMdArrowDropdown } from "react-icons/io";
-import { BsCalendar3 } from "react-icons/bs";
-import { IoRestaurantOutline, IoSchoolOutline, IoDocumentTextOutline } from "react-icons/io5";
-import { RiProfileLine } from "react-icons/ri";
-import { CgProfile } from "react-icons/cg";
-import { MdOutlineLogout } from "react-icons/md";
-import userHookKeycloak from "../../../hooks/userHookKeycloak"
-   
+import styles from './navbar.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import logo from '@/../public/images/logoNovo.png';
+import Login from '../Login/Login';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import { BsCalendar3 } from 'react-icons/bs';
+import {
+  IoRestaurantOutline,
+  IoSchoolOutline,
+  IoDocumentTextOutline,
+} from 'react-icons/io5';
+import { RiProfileLine } from 'react-icons/ri';
+import { CgProfile } from 'react-icons/cg';
+import { MdOutlineLogout } from 'react-icons/md';
+import Error from '../Error/Error';
+import { PiWarningCircleFill } from 'react-icons/pi';
 
 const links = [
   {
     id: 1,
-    name: "Boletins e Frequência",
-    href: "/boletins-e-frequencia",
-    icon: <IoSchoolOutline size={25} />
+    name: 'Boletins e Frequência',
+    href: '/boletins-e-frequencia',
+    icon: <IoSchoolOutline size={25} className={styles.mobileIcon} />,
   },
   {
     id: 2,
-    name: "Calendário",
-    href: "/calendario",
-    icon: <BsCalendar3 size={25} />
+    name: 'Calendário',
+    href: '/calendario',
+    icon: <BsCalendar3 size={25} className={styles.mobileIcon} />,
   },
   {
     id: 3,
-    name: "Cardápio",
-    href: "/cardapio",
-    icon: <IoRestaurantOutline size={25} />
+    name: 'Cardápio',
+    href: '/cardapio',
+    icon: <IoRestaurantOutline size={25} className={styles.mobileIcon} />,
   },
   {
     id: 4,
-    name: "Declarações",
-    href: "/declaracoes",
-    icon: <IoDocumentTextOutline size={25} />
+    name: 'Declarações',
+    href: '/declaracoes',
+    icon: <IoDocumentTextOutline size={25} className={styles.mobileIcon} />,
   },
   {
     id: 5,
-    name: "Matrículas",
-    href: "/matriculas",
-    icon: <RiProfileLine size={45} />
-  }
+    name: 'Matrículas',
+    href: '/matriculas',
+    icon: <RiProfileLine size={25} className={styles.mobileIcon} />,
+  },
 ];
 
 const Navbar = () => {
-  // const logado = userKeycloak.isLoggedIn()
-  // console.log("LOGADO ", logado)
-  // if (logado) {
-  //   userKeycloak.getUsername()
-  // }
   const dispatch = useDispatch<AppDispatch>();
   const userInfoState = useAppSelector((state) => state.authUser);
 
   const [user, setUser] = useState('Nome');
+
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [responsiveLinksOpen, setResponsiveLinksOpen] = useState(false);
+  const [isLogged, setIsLogged] = useState<boolean>(true);
   const pathname = usePathname();
-  
-  const menuItemDelay = .25;
+
+  const menuItemDelay = 0.25;
+
+  const handleLogout = () => {
+    dispatch(authActions.setLogOut());
+    setIsLogged(false);
+    // resetUserToken();
+    userHookKeycloak.doLogout();
+  };
 
   function closeLinks() {
     setResponsiveLinksOpen(false);
   }
 
-  const handleLogout = () => {
-    dispatch(authActions.setLogOut())
-    // resetUserToken();
-    userHookKeycloak.doLogout();
-  };
-
   useEffect(() => {
-    if(userHookKeycloak.isLoggedIn()) {
-      setUser(userInfoState.userInfo.name=="" ? "Anonyomous" : userInfoState.userInfo.name  );
+    if (userHookKeycloak.isLoggedIn()) {
+      setUser(
+        userInfoState.userInfo.name == ''
+          ? 'Anonyomous'
+          : userInfoState.userInfo.name
+      );
     }
+
     document.addEventListener('click', closeLinks);
     return () => {
       document.removeEventListener('click', closeLinks);
-    }
-  })
+    };
+  });
 
   useEffect(() => {
     if (menuOpen) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = "";
+      document.body.style.overflow = '';
     }
-  }, [menuOpen])
+  }, [menuOpen]);
 
   return (
     <header className={styles.header}>
@@ -109,34 +116,55 @@ const Navbar = () => {
           {links.map((link) => {
             const isActive = pathname.startsWith(link.href);
             return (
-              <Link className={isActive ? styles.linkActive : styles.link} href={link.href} key={link.id}>
+              <Link
+                className={isActive ? styles.linkActive : styles.link}
+                href={link.href}
+                key={link.id}
+              >
                 {link.name}
               </Link>
-            )
-          })
-          }
-          <Login />
+            );
+          })}
+          <Login setLoginState={setIsLogged} loginState={isLogged} />
         </div>
 
         {/* Medium Screens Navbar */}
         <div className={styles.responsiveLinks}>
           <div className={styles.responsiveLinksContainer}>
-            <button className={responsiveLinksOpen === true ? `${styles.linksButton} ${styles.linksButtonActive}` : styles.linksButton} onClick={() => setResponsiveLinksOpen(!responsiveLinksOpen)}>
-              <p>Links</p><IoMdArrowDropdown />
+            <button
+              className={
+                responsiveLinksOpen === true
+                  ? `${styles.linksButton} ${styles.linksButtonActive}`
+                  : styles.linksButton
+              }
+              onClick={() => setResponsiveLinksOpen(!responsiveLinksOpen)}
+            >
+              <p>Links</p>
+              <IoMdArrowDropdown />
             </button>
-            <div className={responsiveLinksOpen ? styles.rLinksOpen : styles.rLinks}>
+            <div
+              className={
+                responsiveLinksOpen ? styles.rLinksOpen : styles.rLinks
+              }
+            >
               {links.map((link, i) => (
-                <Link className={styles.rLink} href={link.href} key={`${link} - ${i}`}>{link.name}</Link>
+                <Link
+                  className={styles.rLink}
+                  href={link.href}
+                  key={`${link} - ${i}`}
+                >
+                  {link.name}
+                </Link>
               ))}
             </div>
           </div>
-          <Login />
+          <Login setLoginState={setIsLogged} loginState={isLogged} />
         </div>
 
         {/* Mobile Navbar Button */}
         <div className={styles.mobileNav}>
           <button
-            className={`${styles.iconButton} ${menuOpen ? styles.open : ""}`}
+            className={`${styles.iconButton} ${menuOpen ? styles.open : ''}`}
             onClick={() => setMenuOpen(!menuOpen)}
           >
             <div className={styles.menuIcon}>
@@ -149,39 +177,80 @@ const Navbar = () => {
       </div>
       {/*Mobile Navbar*/}
       <div
-        className={`${styles.menu} ${menuOpen === true ? styles.menuOpen : ""}`}
+        className={`${styles.menu} ${menuOpen === true ? styles.menuOpen : ''}`}
       >
         <div className={styles.menuContainer}>
-          <Link href="/" className={styles.logoMobile} onClick={() => setMenuOpen(false)}>
+          <Link
+            href="/"
+            className={styles.logoMobile}
+            onClick={() => setMenuOpen(false)}
+          >
             <Image className={styles.img} src={logo} alt="Logo" />
           </Link>
-          <div className={`${styles.profile} ${menuOpen ? styles.menuOpenAnim : ""}`} style={{ animationDuration: `${menuItemDelay}s` }}>
-            <CgProfile size={40} />
-            <div className={styles.profileInfo}>
-              <h1>{user}</h1>
-              {/* <p>emaildousuario@gmail.com</p> */}
-            </div>
+          <div
+            className={`${styles.profile} ${
+              menuOpen ? styles.menuOpenAnim : ''
+            }`}
+            style={{ animationDuration: `${menuItemDelay}s` }}
+          >
+            {isLogged ? (
+              <>
+                <CgProfile size={40} />
+                <div className={styles.profileInfo}>
+                  <h1>Nome</h1>
+                  <p>emaildousuario@gmail.com</p>
+                </div>
+              </>
+            ) : (
+              ''
+            )}
           </div>
-          <div className={styles.mobileLinks}>
+          <div
+            className={
+              isLogged
+                ? styles.mobileLinks
+                : `${styles.mobileLinks} ${styles.mobileLinksNotLogged}`
+            }
+          >
             {links.map((link, index) => (
               <Link
-                className={`${styles.mobileLink} ${menuOpen ? styles.menuOpenAnim : ""}`}
+                className={`${styles.mobileLink} ${
+                  menuOpen ? styles.menuOpenAnim : ''
+                }`}
                 href={link.href}
                 key={link.id}
                 onClick={() => setMenuOpen(false)}
                 style={{ animationDuration: `${(index + 2) * menuItemDelay}s` }}
               >
-                {link.icon}{link.name}
+                {link.icon}
+                {link.name}
               </Link>
             ))}
           </div>
           <div className={styles.logout}>
-            <button
-              className={`${styles.logoutButton} ${menuOpen ? styles.menuOpenAnim : ""}`}
-              style={{ animationDuration: `${(links.length + 1) * menuItemDelay}s` }}
-              onClick={handleLogout}>
-              <MdOutlineLogout size={25} /> Sair
-            </button>
+            {isLogged ? (
+              <button
+                className={`${styles.logoutButton} ${
+                  menuOpen ? styles.menuOpenAnim : ''
+                }`}
+                style={{
+                  animationDuration: `${(links.length + 1) * menuItemDelay}s`,
+                }}
+                // onClick={() => setIsLogged(false)}
+                onClick={handleLogout}
+              >
+                <MdOutlineLogout size={25} className={styles.mobileIcon} /> Sair
+              </button>
+            ) : (
+              <Login
+                mobile
+                menuOpen={menuOpen}
+                linksLength={links.length + 1}
+                delay={menuItemDelay}
+                setLoginState={setIsLogged}
+                loginState={isLogged}
+              />
+            )}
           </div>
         </div>
       </div>
